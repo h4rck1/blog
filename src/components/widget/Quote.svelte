e99b34011102329ea141ff0710f575417ca8addc
h4rck1 <h4rck1@gmail.com>
2024-06-20 19:50:23 -0700

    Dont show image

---
+++ src/components/widget/Quote.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { generateQuote } from "@utils/profile-utils"
+
+  let ms = 1000
+  let quote = generateQuote()
+  const createQuote = () => (quote = generateQuote())
+
+  let clear
+  $: {
+    clearInterval(clear)
+    clear = setInterval(createQuote, ms)
+  }
+</script>
+
+<blockquote>
+  <div class="text-center text-neutral-400 mb-2.5 transition">{quote}</div>
+</blockquote>
